fba3939b117b50db7a8b57628c944385e13417ba
tanim-k <tanimkhan877@gmail.com>
2022-03-19 16:49:50 +0600

    react 6 core concepts summary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+      <LoadComments></LoadComments>
+      
+      
     </div>
   );
 }
+// making component 
+function LoadComments() {
+  // state declaration 1.
+  const [comments, setComments] = useState([]);
+  // use effect kuse kora 2.
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => setComments(data))
+  }, [])
+  return (
+    <div>
+      <h2> comments: {comments.length}</h2>
+      {/* show the comments 3: */}
+      {
+        comments.map(comment => <Comment email={comment.email} body={comment.body}></Comment>)
+      }
+     
+    </div>
+  )
+
+}
+
+function Comment(props) {
+  return (
+    <div>
+      <h4>email: {props.email}</h4>
+      <p>{props.body}</p>
+    </div>
+  )
+}
+useEffect(() => {}, [])
+
+
+function Counter() {
+  const [count, setCount] = useState(5);
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => setCount(count - 1);
+  return (
+    <div style={myStyle}>
+      <h1>Counter: <span style={{color: 'red'}}>{count}</span></h1>
+      <button className='button-design' onClick={handleIncrease}>Increase</button>
+      <button className='button-design' onClick={handleDecrease}>Decrease</button>
+    </div>
+  )
+}
+const myStyle= {backgroundColor: 'hotpink'}
 
 export default App;
+      
